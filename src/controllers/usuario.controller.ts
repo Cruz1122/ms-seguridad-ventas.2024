e201b6827a31e9676f5787f7c8350b08ca40817a
Camilo Cruz <juan.cruz37552@ucaldas.edu.co>
2024-03-06 12:47:09 -0500

    Pruebas y ajustes en login

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -196,6 +196,7 @@ export class UsuarioController {
       login.token = '';
       login.estadoToken = false;
       this.repositorioLogin.create(login);
+      usuario.clave = '';
       // notificar al usuario via correo o sms
       return usuario;
     }
@@ -221,12 +222,26 @@ export class UsuarioController {
       let token = this.servicioSeguridad.crearToken(usuario);
       if (usuario) {
         usuario.clave = '';
+        try {
+          this.usuarioRepository
+            .logins(usuario._id)
+            .patch(
+              {estadoCodigo2fa: true, token: token},
+              {estadoCodigo2fa: false},
+            );
+        } catch {
+          console.log(
+            'No se ha almacenado el cambio del estado del token en la base de datos',
+          );
+        }
         return {
           user: usuario,
           token: token,
         };
       }
     }
-    return new HttpErrors[401]('Credenciales incorrectas.');
+    return new HttpErrors[401](
+      'Código de 2fa inválido para el usuario definido.',
+    );
   }
 }
