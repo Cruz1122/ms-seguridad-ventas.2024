ea4c7e69a51580c78905a205f3ea58a1b08780e6
Camilo Cruz <juan.cruz37552@ucaldas.edu.co>
2024-03-06 11:56:33 -0500

    Creación de token JWT en el login

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -19,7 +19,12 @@ import {
   requestBody,
   response,
 } from '@loopback/rest';
-import {Credenciales, Login, Usuario} from '../models';
+import {
+  Credenciales,
+  FactorDeAutenticacionPorCodigo,
+  Login,
+  Usuario,
+} from '../models';
 import {LoginRepository, UsuarioRepository} from '../repositories';
 import {SeguridadUsuarioService} from '../services';
 
@@ -169,7 +174,7 @@ export class UsuarioController {
   @post('identificar-usuario')
   @response(200, {
     description: 'Identificar un usuario por correo y clave',
-    content: {'application/json': {schema: getModelSchemaRef(Credenciales)}},
+    content: {'application/json': {schema: getModelSchemaRef(Usuario)}},
   })
   async identificarUsuario(
     @requestBody({
@@ -193,6 +198,34 @@ export class UsuarioController {
       this.repositorioLogin.create(login);
       // notificar al usuario via correo o sms
       return usuario;
+    }
+    return new HttpErrors[401]('Credenciales incorrectas.');
+  }
+
+  @post('/verificar-2fa')
+  @response(200, {
+    description: 'Validar un código de 2fa',
+  })
+  async VerificarCodigo2fa(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(FactorDeAutenticacionPorCodigo),
+        },
+      },
+    })
+    credenciales: FactorDeAutenticacionPorCodigo,
+  ): Promise<object> {
+    let usuario = await this.servicioSeguridad.validarCodigo2fa(credenciales);
+    if (usuario) {
+      let token = this.servicioSeguridad.crearToken(usuario);
+      if (usuario) {
+        usuario.clave = '';
+        return {
+          user: usuario,
+          token: token,
+        };
+      }
     }
     return new HttpErrors[401]('Credenciales incorrectas.');
   }
